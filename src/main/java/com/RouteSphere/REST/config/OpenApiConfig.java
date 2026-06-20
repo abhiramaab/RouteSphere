d26d94c2e48ae65852e06e7b4341ee8b7461d2c9
Abhiram <abhiram.b@icloud.com>
2026-06-20 22:37:49 +0530

    Reorder OpenAPI configuration for clarity

--- src/main/java/com/RouteSphere/REST/config/OpenApiConfig.java
+++ src/main/java/com/RouteSphere/REST/config/OpenApiConfig.java
@@ -2,19 +2,18 @@ package com.RouteSphere.REST.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-
     @Bean
     public OpenAPI RouteSphereAPI(){
         return new OpenAPI()
                 .info(new Info().title("RouteSphereAPI")
-                      .addServersItem(new Server().url("https://project.routesphere.abhiram.tech"))
                         .description("RouteSphere is a RESTful backend application developed using Spring Boot for logistics and fleet management operations.")
-                        .version("1.6"));
-
+                        .version("1.6"))
+                .addServersItem(new Server().url("https://project.routesphere.abhiram.tech"));
     }
 }
