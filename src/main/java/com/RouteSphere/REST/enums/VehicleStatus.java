3d0b6f17959ec1f3d658fc0ea0270169bce3f7ca
Abhirama <abhiram.b@icloud.com>
2026-06-21 15:29:22 +0530

    Delete src directory

--- src/main/java/com/RouteSphere/REST/enums/VehicleStatus.java
+++
@@ -1,7 +0,0 @@
-package com.RouteSphere.REST.enums;
-
-public enum VehicleStatus {
-    AVAILABLE,
-    IN_TRANSIT,
-    UNDER_SERVICE
-}
--- src/main/java/com/RouteSphere/REST/repository/FuelLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.RouteSphere.REST.repository;
-
-import com.RouteSphere.REST.entity.FuelLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuelLogRepository extends JpaRepository<FuelLog, Long> {
-}
